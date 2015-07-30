82be4b7a16b542d1a8eb562b342edb5df504288b
Carlos Villuendas <carlosvillu@gmail.com>
2015-07-30 21:31:15 +0200

    Updates

---
+++ index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import suntory from '../src';
+import './index.scss';
+
+class Item extends React.Component {
+  render() {
+    return <li className="List-item" data-click>{this.props.text}</li>;
+  }
+}
+
+@suntory({
+  'mouseover [data-click]': function(e) { console.log(e.target) }
+})
+class List extends React.Component {
+  render() {
+    return (
+        <div>
+          <ul>
+            {
+              "Hola mundo, esto es una prueba de suntory".split(' ').map((text, index) => {
+                return (<Item key={index} text={text} />);
+              })
+            }
+          </ul>
+        </div>
+    )
+  }
+}
+
+React.render(<List />, document.getElementById('container'));
