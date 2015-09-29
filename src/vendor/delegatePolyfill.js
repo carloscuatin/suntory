55b05fe91c3f81305875f46ab624f146f45cbd5b
Carlos Villuendas <carlosvillu@gmail.com>
2015-09-29 17:46:20 +0200

    Isomorphic code

--- src/vendor/delegatePolyfill.js
+++ src/vendor/delegatePolyfill.js
@@ -1,6 +1,16 @@
 // Gist from https://gist.github.com/elijahmanor/6452535
 // matches polyfill
-if (Element && !Element.prototype.matches) {
+
+var root;
+if (typeof window !== 'undefined') { // Browser window
+  root = window;
+} else if (typeof self !== 'undefined') { // Web Worker
+  root = self;
+} else { // Other environments
+  root = global;
+}
+
+if (root.Element && !root.Element.prototype.matches) {
     var proto = Element.prototype;
     proto.matches = proto.matchesSelector ||
         proto.mozMatchesSelector || proto.msMatchesSelector ||
@@ -9,18 +19,20 @@ if (Element && !Element.prototype.matches) {
 
 // Gist from https://gist.github.com/paulirish/12fb951a8b893a454b32#gistcomment-1474623
 // Delegate event polyfill
-Node.prototype.on = window.on = function (name, delegate, fn) {
-  if(arguments.length !== 3) {
-    return this.addEventListener(name, arguments[1]);
-  }
-
-  return this.addEventListener(name, function (e) {
-    if(e.target.matches(delegate)){
-      return fn.apply(e.target, arguments);
+if(root.Node){
+  Node.prototype.on = root.on = function (name, delegate, fn) {
+    if(arguments.length !== 3) {
+      return this.addEventListener(name, arguments[1]);
     }
-  })
-};
 
-Node.prototype.off = window.off = function(name, delegate, fn) {
-  return this.removeEventListener(name, fn || delegate);
-};
+    return this.addEventListener(name, function (e) {
+      if(e.target.matches(delegate)){
+        return fn.apply(e.target, arguments);
+      }
+    })
+  };
+
+  Node.prototype.off = root.off = function(name, delegate, fn) {
+    return this.removeEventListener(name, fn || delegate);
+  };
+}
